091bb1cf372294aea3522486e01212378df5d105
Hsuan <qazxcvbnm89@gmail.com>
2021-09-10 21:11:53 +0800

    add variables test

---
+++ Variables/Casting.py
@@ -0,0 +1,34 @@
+x = str(5)   # x will be 1
+y = int(5) # y will be 2
+z = float(5) # z will be 3
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("--------------------------")
+
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("--------------------------")
+
+
+x = "3"
+y = 3
+z = 3.0
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("--------------------------")
+
+# is the same as
+x = "John"
+print(type(x))
+x = 'John'
+print(type(x))
